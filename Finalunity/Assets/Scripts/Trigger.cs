2ced9ee2114398b911fd908f4ef7d46e2037bccd
ChristopherAlly1 <chris123ally@outlook.com>
2016-11-23 17:41:37 -0500

    First Commit
    Added Unity Project to GitHub

---
+++ Finalunity/Assets/Scripts/Trigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour {
+    public GameObject self;
+   void OnCollisionEnter(Collision c)
+    {
+        GameObject hit = c.gameObject;
+        Health health = hit.GetComponent<Health>();
+        if(health != null)
+        {
+            health.TakeDamage(10);
+        }
+        Destroy(self);
+    }
+}
